81fd0aee2760d676f80ccfa04f714ed0827afca3
v.seregin <v.seregin@aitarget.com>
2024-06-12 23:42:19 +0300

    up

--- sandbox/n-body-barnes-hut-cuda/build.sh
+++ sandbox/n-body-barnes-hut-cuda/build.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-nvcc -O1 -arch=sm_75 -use_fast_math ECL-BH_45.cu -o ecl-bh
+
+# nvcc -O1 -arch=sm_75 -use_fast_math ECL-BH_45.cu -o ecl-bh
+nvcc -O1 -arch=sm_50 -use_fast_math ECL-BH_45.cu -o ecl-bh
 
